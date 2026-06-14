cab8ff73d88b98ba36ff132c58796856e352321b
ohwoongg1 <ohwoongg1@gmail.com>
2026-06-14 15:22:53 +0900

    Create kakao-token.js

---
+++ api/kakao-token.js
@@ -0,0 +1,25 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  const { code, redirectUri } = req.body;
+
+  try {
+    const response = await fetch('https://kauth.kakao.com/oauth/token', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams({
+        grant_type: 'authorization_code',
+        client_id: '25b433faf5d746c938cbcc7658f650b0',
+        redirect_uri: redirectUri,
+        code: code
+      })
+    });
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+}
