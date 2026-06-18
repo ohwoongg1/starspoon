9679bac15a47d02f6470b33ec46ab57c539f8fc1
ohwoongg1 <ohwoongg1@gmail.com>
2026-06-18 13:06:17 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-// Service Worker 비활성화 - 캐시 전부 삭제
-self.addEventListener('install', e => { self.skipWaiting(); });
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-  );
-  self.clients.claim();
-});
-self.addEventListener('fetch', e => { e.respondWith(fetch(e.request)); });
